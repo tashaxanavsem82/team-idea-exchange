d18eda6b880b55dcb320d6c0dce4604869dd3b85
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-02 11:43:54 -0500

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const ideaRoutes = require('./routes/ideaRoutes');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use('/api', ideaRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
