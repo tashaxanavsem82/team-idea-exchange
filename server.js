7ec3fb492a55dc670a1d079c57fadf058eb4b41f
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-02 11:43:49 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Team Idea Exchange API!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
